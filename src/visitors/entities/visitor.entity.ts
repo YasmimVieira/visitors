a5ec92d34d370c76d71c58a17cc949a266482d64
YasmimVieira <vieira937@gmail.com>
2026-05-25 21:26:52 -0300

    feat(config): add application and database configuration with Joi validation

--- src/visitors/entities/visitor.entity.ts
+++ src/visitors/entities/visitor.entity.ts
@@ -1,1 +1,16 @@
-export class Visitor {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Visitor {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    visitDate!: Date;
+}
