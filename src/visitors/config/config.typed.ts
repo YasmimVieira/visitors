bee7d53f1e6f435676b8cd18209f16e9979aac40
YasmimVieira <vieira937@gmail.com>
2026-05-25 21:55:42 -0300

    fix(config): rename database user and password keys for consistency

--- src/visitors/config/config.typed.ts
+++ src/visitors/config/config.typed.ts
@@ -11,7 +11,7 @@ export const appConfigSchema = Joi.object({
   APP_MESSAGE_PREFIX: Joi.string().default('Hello '),
   DB_HOST: Joi.string().default('localhost'),
   DB_PORT: Joi.number().default(5432),
-  DB_USER: Joi.string().required(),
-  DB_PASS: Joi.string().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
 });
