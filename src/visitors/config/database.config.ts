23a63c96ff34ff1175eb15d66acca0beddfd6eed
YasmimVieira <vieira937@gmail.com>
2026-05-25 21:41:01 -0300

    fix(config): update database configuration keys to match schema

--- src/visitors/config/database.config.ts
+++ src/visitors/config/database.config.ts
@@ -7,8 +7,8 @@ export const typeOrmConfig = registerAs(
     type: 'postgres',
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT ?? '5432'),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
     synchronize: Boolean(process.env.DB_SYNC ?? false),
     ssl: {
